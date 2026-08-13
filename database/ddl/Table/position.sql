d43fcea9df4eae7834b1419949c92f4c5a3ed1aa
Kaique Pires <kaiqueroberto.pires@gmail.com>
2026-08-12 21:41:10 -0300

    refactor: standardize database ddl directory naming

--- database/ddl/Table/position.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE position (
-   position_id   NUMBER(10) NOT NULL,
-   position_name VARCHAR2(50 CHAR) NOT NULL
-);
-
-ALTER TABLE position ADD CONSTRAINT position_pk PRIMARY KEY ( position_id );
